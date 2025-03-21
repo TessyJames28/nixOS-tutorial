23f7299e69f7285b226914bccab0b76c5866e063
TessyJames28 <tessyjames28@gmail.com>
2025-03-21 20:42:35 +0000

    module evaluation

---
+++ eval.nix
@@ -0,0 +1,12 @@
+# This file evaluate the module in default.nix
+
+let
+    nixpkgs = fetchTarball "https://github.com/NixOS/nixpkgs/tarball/nixos-23.11";
+    pkgs = import nixpkgs { config = {}; overlays = []; };
+in
+    pkgs.lib.evalModules {
+        modules = [
+            ({ config, ... }: { config._module.args = { inherit pkgs; }; })
+            ./default.nix
+        ];
+    }
