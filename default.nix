a20492ed34378feedb0e8aa25f42d645478cfb57
TessyJames28 <tessyjames28@gmail.com>
2025-03-21 20:42:13 +0000

    default file

---
+++ default.nix
@@ -0,0 +1,17 @@
+{ pkgs, lib, ... }: { # Declaring options
+    options = {
+        scripts.output = lib.mkOption {
+            type = lib.types.package;
+            };
+        };
+        
+        config = {
+            scripts.output = pkgs.writeShellApplication {
+                name = "map";
+                runtimeInputs = with pkgs; [ curl feh ];
+                text = ''
+                    ${./map.sh} size=640x640 scale=2 | feh -
+                '';
+            };
+        };
+    }
