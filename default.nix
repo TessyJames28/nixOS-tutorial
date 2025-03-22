30f8e14aa50793c9c851d495337c5e7def2de5ca
TessyJames28 <tessyjames28@gmail.com>
2025-03-22 03:13:33 +0000

    Added geocode as scripts.geocode in the option

--- default.nix
+++ default.nix
@@ -1,17 +1,63 @@
-{ pkgs, lib, ... }: { # Declaring options
+# Declaring options. config makes option values available to a module
+{ pkgs, lib, config, ... }: {
+
+    # import a file that handles location pins and other markers on the map
+    imports = [
+        ./market.nix
+    ];
+
     options = {
         scripts.output = lib.mkOption {
             type = lib.types.package;
+        };
+
+        # option for geocode to translate text map location to coordinates
+        scripts.geocode = lib.mkOption {
+            type = lib.types.package;
+        }; 
+
+        requestParams = lib.mkOption {
+            type = lib.types.listOf lib.types.str;
+        };
+        map = {
+            zoom = lib.mkOption {
+                type = lib.types.nullor lib.types.int;
+                default = 10; # set automatic zoom upon image opening
+            };
+
+            center = lib.mkOption {
+                type = lib.types.nullOr lib.types.str;
+                default = "switzerland";
             };
         };
         
-        config = {
-            scripts.output = pkgs.writeShellApplication {
-                name = "map";
-                runtimeInputs = with pkgs; [ curl feh ];
-                text = ''
-                    ${./map.sh} size=640x640 scale=2 | feh -
-                '';
-            };
+    };
+        
+    config = {
+        scripts.geocode = pkgs.writeShellApplication {
+            name = "geocode";
+            runtimeInputs = with pkgs; [ curl jq ];
+            text = ''exec ${./geocode.sh} "$@"'';
         };
+
+        scripts.output = pkgs.writeShellApplication {
+            name = "map";
+            runtimeInputs = with pkgs; [ curl feh ];
+            text = ''
+                ${./map} ${lib.concatStringsSep " "
+                config.requestParams} | feh -
+            '';
+        };
+
+        requestParams = [
+            "size=640x640"
+            "scale=2"
+            (lib.mkif (config.map.zoom != null)
+                "zoom=${toString config.map.zoom}")
+            (lib.mkif (config.map.center != null)
+                "center=\"$(${config.scripts.geocode}/bin/geocode ${
+                    lib.escapeShellArg config.map.center
+                })\"")
+        ];
+    };
     }
